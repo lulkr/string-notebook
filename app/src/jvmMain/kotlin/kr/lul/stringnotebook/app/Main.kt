d85d795240c622cf59399698af8a171b178e815e
Just Burrow <just.burrow@lul.kr>
2025-06-07 12:59:36 +0900

    🖥️ Desktop 앱 실행할 때 프로세스 정보 싱글턴 생성.

--- app/src/jvmMain/kotlin/kr/lul/stringnotebook/app/Main.kt
+++ app/src/jvmMain/kotlin/kr/lul/stringnotebook/app/Main.kt
@@ -2,20 +2,53 @@ package kr.lul.stringnotebook.app
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kr.lul.logger.Logger
+import kr.lul.logger.d
+import kr.lul.stringnotebook.domain.foundation.Configuration.ID_PREFIX_APP_PROCESS
+import kr.lul.stringnotebook.domain.foundation.Configuration.generateId
+import kr.lul.stringnotebook.model.Process
 import kr.lul.stringnotebook.navigation.Root
 import kr.lul.stringnotebook.navigation.compose.rememberBaseNavigator
 import kr.lul.stringnotebook.navigation.navigationModule
 import kr.lul.stringnotebook.navigation.navigator.SplashNavigator
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
-fun main() = application {
-    startKoin {
-        modules(navigationModule)
+@ExperimentalUuidApi
+@OptIn(ExperimentalStdlibApi::class)
+fun main() {
+    val logger = Logger("main")
+    val process: Process = object : Process {
+        override val startedAt: Instant = Clock.System.now()
+
+        override val id: Uuid = ID_PREFIX_APP_PROCESS.generateId(startedAt.epochSeconds)
+
+        override fun toString() = listOf(
+            "id=$id",
+            "startedAt=$startedAt"
+        ).joinToString(", ", "Process(", ")")
     }
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "String Notebook",
-    ) {
-        Root(rememberBaseNavigator(SplashNavigator))
+
+    application {
+        startKoin {
+            modules(
+                module {
+                    single { process }
+                },
+                navigationModule
+            )
+            logger.d { "#main check koin : process=${koin.get<Process>()}" }
+        }
+
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "String Notebook",
+        ) {
+            Root(rememberBaseNavigator(SplashNavigator))
+        }
     }
 }
