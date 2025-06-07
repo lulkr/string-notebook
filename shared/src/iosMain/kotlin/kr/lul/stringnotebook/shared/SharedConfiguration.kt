6ebc9774bea8ecc4a6db564d8be05550ab9b7188
Just Burrow <just.burrow@lul.kr>
2025-06-07 13:06:18 +0900

    📱 iOS 앱 실행할 때 프로세스 정보 싱글턴 생성.

--- shared/src/iosMain/kotlin/kr/lul/stringnotebook/shared/SharedConfiguration.kt
+++ shared/src/iosMain/kotlin/kr/lul/stringnotebook/shared/SharedConfiguration.kt
@@ -1,13 +1,40 @@
 package kr.lul.stringnotebook.shared
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kr.lul.logger.Logger
+import kr.lul.stringnotebook.domain.foundation.Configuration.ID_PREFIX_APP_PROCESS
+import kr.lul.stringnotebook.domain.foundation.Configuration.generateId
+import kr.lul.stringnotebook.model.Process
 import kr.lul.stringnotebook.navigation.navigationModule
 
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+@ExperimentalUuidApi
+@OptIn(ExperimentalStdlibApi::class)
 object SharedConfiguration {
+    private val logger = Logger("SharedConfiguration")
+
     fun initialize() {
+        logger.i("#initialize called : Configuration=${kr.lul.stringnotebook.domain.foundation.Configuration}")
+
+        val process: Process = object : Process {
+            override val startedAt: Instant = Clock.System.now()
+
+            override val id: Uuid = ID_PREFIX_APP_PROCESS.generateId(startedAt.epochSeconds)
+
+            override fun toString() = listOf(
+                "id=$id",
+                "startedAt=$startedAt"
+            ).joinToString(", ", "Process(", ")")
+        }
         startKoin {
-            modules(navigationModule)
+            modules(module { single { process } }, navigationModule)
+
+            logger.d("#initialize check koin : process=${koin.get<Process>()}")
         }
     }
 }
