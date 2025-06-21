ad24aab5ce45cda7d2f42c18c95a230bf5ed7e7f
Just Burrow <just.burrow@lul.kr>
2025-06-21 12:26:59 +0900

    ✨ 활성화한 노드 바꾸기.

--- ui/src/commonMain/kotlin/kr/lul/stringnotebook/ui/organism/Node.kt
+++ ui/src/commonMain/kotlin/kr/lul/stringnotebook/ui/organism/Node.kt
@@ -7,6 +7,8 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import kr.lul.stringnotebook.domain.event.ActivateEvent
 import kr.lul.stringnotebook.domain.foundation.EventProcessor
@@ -30,6 +32,11 @@ fun Node(
 
     val colors = NodeDefaults.colors()
 
+    if (state != context.active) {
+        LocalFocusManager.current
+            .clearFocus()
+    }
+
     OutlinedTextField(
         value = state.text,
         onValueChange = { },
@@ -37,6 +44,16 @@ fun Node(
             .clickable(!context.lock && state != context.active) {
                 logger.d("#Node.onClick args : state=$state, context=$context")
                 processor(ActivateEvent(state.id))
+            }
+            .onFocusChanged { focus ->
+                if (!context.lock) {
+                    when {
+                        focus.isFocused && state != context.active -> {
+                            logger.d("#Node.onFocusChanged : focus=$focus")
+                            processor(ActivateEvent(state.id))
+                        }
+                    }
+                }
             }
             .background(if (state == context.active) colors.background else colors.inactiveBackground)
             .padding(16.dp),
