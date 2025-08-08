fe2e67f5c6639c013ef2b08cd92e358be9900ae1
Just Burrow <just.burrow@lul.kr>
2025-08-08 23:18:50 +0900

    🏷️ 빈 노트북 모델 추가.

---
+++ model/src/commonMain/kotlin/kr/lul/stringnotebook/model/NotebookModelImpl.kt
@@ -0,0 +1,28 @@
+package kr.lul.stringnotebook.model
+
+import kr.lul.logger.Logger
+import kr.lul.stringnotebook.data.entity.Notebook
+import kr.lul.stringnotebook.data.repository.NotebookRepository
+import kr.lul.stringnotebook.domain.foundation.Notebook
+import kotlin.uuid.ExperimentalUuidApi
+
+@ExperimentalUuidApi
+class NotebookModelImpl(
+    private val repository: NotebookRepository
+) : NotebookModel {
+    private val logger = Logger("NotebookModelImpl")
+
+    /**
+     * 새로운 노트북을 생성한다.
+     *
+     * @return 생성된 노트북 객체.
+     */
+    override suspend fun create(): Notebook {
+        logger.d("#create called.")
+
+        val notebook = repository.create(Notebook())
+
+        logger.d("#return : $notebook")
+        return notebook
+    }
+}
