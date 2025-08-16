0aa3dce8048e2c8fa516eff08fd2c51b9eef1585
Just Burrow <just.burrow@lul.kr>
2025-08-16 11:23:31 +0900

    🏷️ 직사각형 크기 속성 추가.

---
+++ domain/src/commonMain/kotlin/kr/lul/stringnotebook/domain/property/SizeProperty.kt
@@ -0,0 +1,34 @@
+package kr.lul.stringnotebook.domain.property
+
+import kr.lul.stringnotebook.domain.foundation.CompositeProperty
+import kr.lul.stringnotebook.domain.type.SizeType
+import kr.lul.stringnotebook.domain.type.SizeType.HEIGHT
+import kr.lul.stringnotebook.domain.type.SizeType.WIDTH
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+/**
+ * 직사각형 크기.
+ */
+@ExperimentalStdlibApi
+@ExperimentalUuidApi
+class SizeProperty(
+    id: Uuid,
+    name: String,
+    /**
+     * 폭
+     */
+    val width: LengthProperty,
+    /**
+     * 높이
+     */
+    val height: LengthProperty
+) : CompositeProperty(
+    id = id,
+    type = SizeType,
+    name = name,
+    children = mapOf(
+        WIDTH to width,
+        HEIGHT to height
+    )
+)
