8d2c5d89fb09194cfeda949d9066bc84bcb8979d
Just Burrow <just.burrow@lul.kr>
2025-09-23 10:57:35 +0900

    ✨ 상태 홀더 유틸리티 함수 추가.

--- state/src/commonMain/kotlin/kr/lul/stringnotebook/state/atom/BorderState.kt
+++ state/src/commonMain/kotlin/kr/lul/stringnotebook/state/atom/BorderState.kt
@@ -1,5 +1,7 @@
 package kr.lul.stringnotebook.state.atom
 
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -36,3 +38,41 @@ data class BorderState(
         shape: Shape = this.shape
     ) = BorderState(width, SolidColor(color), shape)
 }
+
+fun CircleBorderState(width: Dp, brush: Brush = SolidColor(Color.Unspecified)) = BorderState(width, brush, CircleShape)
+
+fun CircleBorderState(width: Dp, color: Color = Color.Unspecified) = BorderState(width, SolidColor(color), CircleShape)
+
+fun RoundedCornerBorderState(
+    width: Dp,
+    cornerRadius: Dp,
+    brush: Brush = SolidColor(Color.Unspecified)
+) = BorderState(width, brush, RoundedCornerShape(cornerRadius))
+
+fun RoundedCornerBorderState(
+    width: Dp,
+    cornerRadius: Dp,
+    color: Color = Color.Unspecified
+) = BorderState(width, SolidColor(color), RoundedCornerShape(cornerRadius))
+
+fun RoundedCornerBorderState(
+    width: Dp,
+    topStartRadius: Dp,
+    topEndRadius: Dp,
+    bottomEndRadius: Dp,
+    bottomStartRadius: Dp,
+    brush: Brush = SolidColor(Color.Unspecified)
+) = BorderState(width, brush, RoundedCornerShape(topStartRadius, topEndRadius, bottomEndRadius, bottomStartRadius))
+
+fun RoundedCornerBorderState(
+    width: Dp,
+    topStartRadius: Dp,
+    topEndRadius: Dp,
+    bottomEndRadius: Dp,
+    bottomStartRadius: Dp,
+    color: Color = Color.Unspecified
+) = BorderState(
+    width,
+    SolidColor(color),
+    RoundedCornerShape(topStartRadius, topEndRadius, bottomEndRadius, bottomStartRadius)
+)
